8bb38d0f8982aef078797e28d02c0c390056ef81
brayevalerien <valerien.braye@telecom-sudparis.eu>
2024-07-10 17:38:52 +0200

    added some more option and wrote proper readme

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # ASCII-QR-Code-generator
 Generate ASCII art of QR codes and choose the characters to use in the output.
+
+## Installation
+> This code is written in Python. To download the code and run it, make sure [Git](https://git-scm.com/) and [Python](https://www.python.org/) are installed on your machine before installing this project.
+
+Start by clonning this repo and `cd` into it:
+```bash
+git clone https://github.com/brayevalerien/ascii-qr-code-generator
+cd ASCII-QR-Code-generator
+```
+
+Then install the `qrcode` library:
+```bash
+pip install qrcode
+```
+
+## Usage
+Run the `main.py` script with Python. Here is the usage string:
+```
+ASCII QR Code generator [-h] --content CONTENT [--characters CHARACTERS] [--filepath FILEPATH] [--random RANDOM] [--correction CORRECTION]
+
+A QR Code generator that outputs ASCII art, using specified characters.
+
+options:
+  -h, --help            show this help message and exit
+  --content CONTENT     Content to include in the generated QR code (usually an URL).
+  --characters CHARACTERS
+                        String containing the characters to use in the output.
+  --filepath FILEPATH   If not None, will print the output to a file at the path (creating the file if not already existent). Otherwise, prints the output to stdout.
+  --random RANDOM       If set to True, characters will be picked at random in the given string.
+  --correction CORRECTION
+                        Error correction level of the generated QR code.
+```
+
+> [!TIP] Character selection
+> Use characters that are dark enough (e.g. `#` is dark, `.` is light) to ensure the resulting QR code is scannable.
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+from random import randint
 
 import qrcode
 
@@ -6,36 +7,51 @@ import qrcode
 def parse_arguments():
     parser = argparse.ArgumentParser("ASCII QR Code generator",
                                      description="A QR Code generator that outputs ASCII art, using specified characters.")
-    parser.add_argument("--content", required=True, type=str)
-    parser.add_argument("--characters", required=False, default="#", type=str)
-    parser.add_argument("--filepath", required=False, default=None, type=str)
+    parser.add_argument("--content", required=True, type=str,
+                        help="Content to include in the generated QR code (usually an URL).")
+    parser.add_argument("--characters", required=False, default="#", type=str,
+                        help="String containing the characters to use in the output.")
+    parser.add_argument("--filepath", required=False, default=None, type=str,
+                        help="If not None, will print the output to a file at the path (creating the file if not already existent). Otherwise, prints the output to stdout.")
+    parser.add_argument("--random", required=False, default=None, type=str,
+                        help="If set to True, characters will be picked at random in the given string.")
+    parser.add_argument("--correction", required=False, default="medium", type=str,
+                        help="Error correction level of the generated QR code.")
     return parser.parse_args()
 
 
-def generate_qrcode_data(content: str) -> list[list[bool]]:
+def generate_qrcode_data(content: str, correction: str = "medium") -> list[list[bool]]:
     """
     Create the QR code data (bits) 
 
     Args:
         content (str): content to include in the generated QR code.
+        correction (str): error correction level of the generated QR code, can be of "low" (7%), "medium" (15%), "quality" (25%), "high" (30%). Defaults to "medium".
 
     Returns:
         list[list[bool]]: a matrix of booleans, True represents a black cell and False a white one.
     """
-    # TODO: provide more control on the generated qr code (error correction level etc.)
-    qr = qrcode.QRCode(border=0)
+    correction_levels = {
+        "low": qrcode.constants.ERROR_CORRECT_L,
+        "medium": qrcode.constants.ERROR_CORRECT_M,
+        "quality": qrcode.constants.ERROR_CORRECT_Q,
+        "high": qrcode.constants.ERROR_CORRECT_H
+    }
+    qr = qrcode.QRCode(
+        border=0, error_correction=correction_levels[correction.lower()])
     qr.add_data(content)
     qr.make()
     return qr.modules
 
 
-def qrcode_data_to_ascii(qr_data: list[list[bool]], characters: str = "#") -> list[str]:
+def qrcode_data_to_ascii(qr_data: list[list[bool]], characters: str = "#", random: bool = False) -> list[str]:
     """
     Converts a QR data (bits representing each cell in the QR code) to a matrix of characters (a list of strings) included in the characters arguments, that represent the QR code.
 
     Args:
         qr_data (list[list[bool]]): bits representing each cell in the QR code.
         characters (str): string containing the characters to use in the output. Defaults to "#".
+        random (bool): if kept to False, the characters will be used in the order they are given. If set to True, characters will be picked at random in the given string.
 
     Returns:
         list[str]: the ASCII representation of the QR code, ready to be printed to the stdout or into a text file.
@@ -43,17 +59,25 @@ def qrcode_data_to_ascii(qr_data: list[list[bool]], characters: str = "#") -> li
     if not characters:
         raise ValueError(
             "Character list is empty, please provide at least one character to use.")
-    i = 0  # pointer to the character to use in the character string
     qr_ascii = []
+    char_count = len(characters)
+    # Quick and dirty nested for loops. Works well enough with qr codes for the moment.
     for line in qr_data:
+        i = 0  # pointer to the character to use in the character string
         ascii_line = ""
         for bit in line:
-            # Do it twice since characters are approx. 2 times taller than they are wide
-            # Quick and dirty nested for loops. Works well enough for qr codes for the moment.
-            ascii_line += characters[i] if bit else " "
-            i = (i+1) % len(characters) if bit else i
-            ascii_line += characters[i] if bit else " "
-            i = (i+1) % len(characters) if bit else i
+            if bit:
+                # Do it twice since characters are approx. 2 times taller than they are wide
+                ascii_line += characters[randint(0,
+                                                 char_count-1) if random else i]
+                if not random:
+                    i = (i+1) % char_count
+                ascii_line += characters[randint(0,
+                                                 char_count-1) if random else i]
+                if not random:
+                    i = (i+1) % char_count
+            else:
+                ascii_line += "  "
         qr_ascii.append(ascii_line)
     return qr_ascii
 
@@ -64,7 +88,7 @@ def print_output(qr_ascii: list[str], filepath: str = None) -> None:
 
     Args:
         qr_ascii (list[str]): content to print.
-        filepath (str, optional): ignored if None. If not None, will print the output to a file at the path (creating the file if not already existent). Defaults to None.
+        filepath (str, optional): If not None, will print the output to a file at the path (creating the file if not already existent). Otherwise, prints the output to stdout. Defaults to None.
     """
     output = "\n".join(qr_ascii)
     if filepath is None:
@@ -79,6 +103,6 @@ if __name__ == "__main__":
 
     qr_data = generate_qrcode_data(args.content)
 
-    qr_ascii = qrcode_data_to_ascii(qr_data, args.characters)
+    qr_ascii = qrcode_data_to_ascii(qr_data, args.characters, args.random)
 
     print_output(qr_ascii, args.filepath)
